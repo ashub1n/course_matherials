2bff36c52d5b38a2403fe21b47d85de31ab16372
Alex Shubin <0992shubin@gmail.com>
2022-04-09 17:37:13 +0300

    init

---
+++ 2. types.js
@@ -0,0 +1,14 @@
+const s = 'String';
+const int = 123;
+const maxSafe = Number.MAX_SAFE_INTEGER;
+const bigInt = 123123123123123123123127635172653716253761527365172653716523765127356712653716527365172635712653n;
+const booleanVal = true; // false [1, 0]
+let empty; //undefined
+let empty2 = null;
+// Array, Object
+//Symbol
+//const id = Symbol('123123123');
+//const id2 = Symbol('123123123');
+
+//1 == 1
+//console.log(1 == 1, id == id2);
